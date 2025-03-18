d22416c45040443b27a5da9953b2022af591779a
Maui <babamaui@msu.edu>
2025-03-17 20:11:38 -0400

    mat processing fix

--- util/mat_processing.py
+++ util/mat_processing.py
@@ -13,14 +13,13 @@ def process_matfile(file_name):
         filename = data_dict['full_path'][0][i][0]
         print(filename)
         filename_dict[filename] = {
-            'age': data_dict['age'][0][i][0],
-            'dob': data_dict['dob'][0][i][0],
-            'photo_taken': data_dict['photo_taken'][0][i][0],
-            'gender': data_dict['gender'][0][i][0],
-            'name': data_dict['name'][0][i][0],
-            'face_location': data_dict['face_location'][0][i][0],
-            'face_score': data_dict['face_score'][0][i][0],
-            'second_face_score': data_dict['second_face_score'][0][i][0]
+            'dob': data_dict['dob'][0][i],
+            'photo_taken': data_dict['photo_taken'][0][i],
+            'gender': data_dict['gender'][0][i],
+            'name': data_dict['name'][0][i],
+            'face_location': data_dict['face_location'][0][i],
+            'face_score': data_dict['face_score'][0][i],
+            'second_face_score': data_dict['second_face_score'][0][i]
         }
 
     return filename_dict
